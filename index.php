53f1e4dddf4dd19d0f5972489741bb431b6a59e2
Sourabh Kumar Agrawal <sourabhbitian@gmail.com>
2013-01-18 02:48:51 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,290 @@
+    <LINK rel="shortcut icon" type='image/x-icon' 
+href="image/ganesha.gif">
+<script>
+
+if(navigator.appName=="Microsoft Internet Explorer")
+alert("Best View this site in Mozila firfox/google Chrome")
+
+</script>
+
+
+<script type='text/javascript' src='js/jquery-1.2.3.min.js'></script>
+<script type='text/javascript' src='js/menu.js'></script>
+<link rel="stylesheet" href="style1.css" type="text/css"  />
+<style>
+body
+{
+text-align:center;
+}
+#nav
+{
+
+background-color:#FFA500;
+float:center;
+font-size:17;
+width:auto;
+list-style:none;
+padding:9;
+position:absolute;
+top:460;
+left:230;
+
+
+}
+#nav li a
+{
+background-color:#5a0002;
+float:left;
+border-right:5px solid #617C58;
+padding:10;
+
+
+z-index:10;
+
+}
+#nav li a:hover
+{
+background-color:#FDD017;
+text-decoration:underline;
+color:#0000FF;
+}
+#bigad
+{
+background-image:url(images/300x250.jpg);padding:5px;width:300px;height:200px;border:1px solid black;
+}
+
+#sav
+{z-index:5;
+}
+</style>
+
+
+<ul id="nav">
+    <li><a href="index.php" ><b> Home</b></a></li>
+    <li><a href="#"><b> Ganeshotsav </b></a>
+<ul id='sav'>
+                  <li><a href="about.php" ><b>About </b></a></li>
+<li><a href="#" ><b>History </b></a>
+<ul id='sav'>
+                    <li><a href="index.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girls.php"><b>Girls Hostel</b></a></li>
+  				
+                   
+              </ul>
+
+					
+                   
+              </ul>
+    
+            
+    
+
+
+<li><a href='down.php?fname=events.docx'><b>Events</b></a></li>
+
+
+<li><a href='upload.php'><b>Photos</b></a>
+<ul id='sav'>
+                    <li><a href="upload.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girl.html"><b>Girls Hostel</b></a></li>
+					
+                   
+              </ul>
+
+
+
+</ul>
+
+					 </table>
+
+<html>
+
+<head>
+
+
+
+
+
+		
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>Ganeshotsava 2kxii</title>
+
+		<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+		<script type="text/javascript" src="hello.js"></script>
+		<script type="text/javascript" src="js/jquery.gallerax-0.2.js"></script>
+		<script type="text/javascript">
+$.gallerax({
+							outputSelector: 		'#container img',				
+							thumbnailsSelector:		'#thumbnails img',		
+							captionSelector:		'#toppost .line',			
+							captionLines:			3,							
+							fade: 					'fast',						
+							advanceFade:			'slow',						
+							advanceDelay:			4000,						
+							advanceResume:			12000,						
+
+						});
+</script>
+
+		
+</head>
+<body>
+   
+<br>
+<center>
+<embed src="web_ganesh.swf" width="855" height="142.5" margin=10px solid green></embed></td>
+</center>
+                
+            </tr>
+        </table></div>
+		</div>	
+	</div>
+</div>
+
+
+
+
+
+<div id="slideshow">
+	<div id="slideshow_inner">
+		<div class="content">
+		
+				<div id="thumbnails">
+					<ul>
+						<li><a href="#"><img src="image/9.jpg" alt="" title="Ganeshotsav 2kxii;  Welcome to the Ganeshotsav 2kxii Website <br><br>
+Showing Photos previous year Ganeshotsav celebrations..
+
+ " class="active" /></a></li>
+
+						<li><a href="#"><img src="image/2.jpg" alt="" title="Ganeshotsav 2kxii;  At the time of Ganesha Visarjan" /></a></li>
+						<li><a href="#"><img src="image/3.jpg" alt="" title="Ganeshotsav 2kxii;Shri I.P.Mishra sir Inaugrated the Ganeshotsav Festival 2011 " /></a></li>
+						<li><a href="#"><img src="image/4.jpg" alt="" title="Ganeshotsav 2kxii;At the time of  Aarti..." /></a></li>
+						<li><a href="#"><img src="image/5.jpg" alt="" title="Ganeshotsav 2kxii;  " /></a></li>
+						<li><a href="#"><img src="image/6.jpg" alt="" title="Ganeshotsav 2kxii;"/></a></li>
+	
+</ul>
+					
+				</div>
+				
+				<div id="container">
+					<div class="container_inner"><a href="#" title=""><img src="image/9.jpg" alt="" /></a></div>
+				</div>
+				
+				<div id="toppost">
+					<h2><a href="" title=""><span class="line"></span></a></h2>
+					<p class="date line2"></p>
+					<span class="line3">
+						
+					</span>
+					<p class="excerpt"><a href="" title=""></a></p>
+				</div>
+				
+				
+				<div style="clear: both;">&nbsp;</div>
+		
+		</div>	
+	</div>
+</div>
+
+
+<div id="content_wrapper">
+	<div id="content">
+		<div class="content">
+		
+			<div id="post">
+				<p class="date"></p>
+				<h2><a href="#" title=""></a></h2>
+
+				<div class="postimg"><div class="postimg_inner"><a href="#" title=""><img src="images/ganesha.jpg" width='600' height='350' alt="" /></a></div></div>
+				<br />
+<style>
+p.excerpt {text-align:justify;text-indent:50px;font face="verdana"}
+</style>
+				
+				<p class="excerpt"><b>As per the 26 Year old Tradition, The Festive season of ganeshotsav is celebrated in BIT Boys Hostel Every Year with zeal & pure devotion.
+Every student contributes a part of his talent before lord ganesh in This 10 days event.
+Every year we come up with a bunch of exciting & exclusive competitions which seprates bit hostel from Other college hostels.
+The entire work of Decoration of pandaals and puja is conducted by  hostel students without support of any external body.
+we also organise open BHANDARA on the night before Ganesha Visarjan.
+come........feel and enjoy the saga of Ganeshotsav...</b></p>
+<b><a href='about.php'>Read more</b></a>
+			</div>
+
+
+
+
+
+	
+			<div id="sidebar">
+	</a></marquee>
+			
+<h1 align='center'  ><u><b>Live News</b></u></h1>
+ 
+
+<bgcolor='red'>
+<marquee direction='up' onmouseover="this.stop()" onmouseout="this.start()" scrollamount="6" direction="up" align="center"> <b>Live news from BIT-Hostel.....<br><br>
+<b>Poster Making Competition</b> 
+(open for all Boys & Girls)<br />
+Last Date Of Submission: 17-09-2012</b><br ><br>
+<img src='image/poster.jpg' width='100' height='100'>
+<img src='image/poster1.jpg' width='100' height='100'>
+</marquee>
+</body>
+
+
+ 
+</marquee>
+</div></ul>
+<br>
+<br>
+
+
+
+
+
+
+<center>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="https://www.facebook.com/pages/Ganeshotsav-2kxii/375197382549198?sk=info" data-send="true" data-width="250" data-height="250"data-show-faces="true"></div>
+</center>
+
+</div>
+			<div style="clear: both;">&nbsp;</div>
+		
+		</div>
+	</div>
+</div>
+
+<div id="footer">
+
+	<div class="inner"></div>
+		<div class="content">
+
+<div align='center'><img src='http://www.hit-counts.com/counter.php?t=27&digits=5&ic=1000&cid=1141533' border='0' alt=''></a><BR></div>
+
+
+		
+		<b><div style="text-align: center;"><u>Designed By</u><Br><a href="http://www.facebook.com/sourabhbitian" target='_blank' title="Sourabh Kumar Agrawal">Sourabh Kumar Agrawal</a><BR>sourabhbitian@gmail.com</div></b>
+<br><br>
+</body>    
+</html>
+
+
+
+
+
+
+    
