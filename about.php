2005f8d73114ac9e0853573eb1f15878016d2682
Sourabh Kumar Agrawal <sourabhbitian@gmail.com>
2013-01-18 02:49:31 -0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,146 @@
+<link rel="stylesheet" type="text/css" href="style.css" />
+<style>
+<body>
+</body>
+
+
+
+</style>
+
+<?php
+
+include "logo.php";
+?>
+
+<div id='div1' style="background: url(images/bg3.jpg) no-repeat top left;width: 1200px;
+  height: 30px;
+	margin: 0px auto;
+	padding: 0;"></div>
+
+
+
+
+
+
+
+
+</html>
+
+</center>
+ <script type='text/javascript' src='jquery-1.2.3.min.js'></script>
+<script type='text/javascript' src='menu.js'></script>
+<link rel="stylesheet" href="style1.css" type="text/css"  />
+<style>
+
+#nav
+{
+
+background-color:#FFA500;
+float:center;
+font-size:17;
+width:auto%;
+list-style:none;
+padding:9;
+position:absolute;
+top:200;
+left:430;
+
+
+}
+#nav li a
+{
+background-color:#5a0002;
+float:left;
+border-right:5px solid #617C58;
+padding:10;
+
+
+z-index:1;
+
+}
+#nav li a:hover
+{
+background-color:#FDD017;
+text-decoration:underline;
+color:#0000FF;
+
+
+}
+
+#sav
+{z-index:1;
+}
+</style>
+
+</style>
+</style>
+
+
+<ul id="nav">
+    <li><a href="index.php" ><b> Home</b></a></li>
+    <li><a href="#"><b> Ganeshotsav </b></a>
+<ul id='sav'>
+                  <li><a href="about.php" ><b>About </b></a></li>
+<li><a href="#" ><b>History </b></a>
+<ul id='sav'>
+                    <li><a href="index.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girls.php"><b>Girls Hostel</b></a></li>
+					
+                   
+              </ul>
+
+					
+                   
+              </ul>
+    
+            
+    
+
+
+<li><a href='down.php?fname=events.docx'><b>Events</b></a></li>
+
+
+<li><a href='upload.php'><b>Photos</b></a>
+<ul id='sav'>
+                    <li><a href="upload.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girl.html"><b>Girls Hostel</b></a></li>
+					
+                   
+              </ul>
+
+
+</ul>
+
+					 </table>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+
+
+
+
+
+<b><u><h1 style='color:yellow;font-size:30' align='center'>About Ganeshotsava</a></u></b>
+
+<br>
+<br>
+
+
+
+					 </table>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
+<body>
+  <iframe src="1.html" width="80%" height="590" id='frameDemo'></iframe> 
+<script>$("#frameDemo").contents().find("a").css("background-color","#BADA55");</script>
+
+
+
+
