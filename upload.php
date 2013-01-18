d2e0f4eb856b827c0e2bba6fae6862c15085748f
Sourabh Kumar Agrawal <sourabhbitian@gmail.com>
2013-01-18 02:51:59 -0800

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,234 @@
+<link rel="stylesheet" type="text/css" href="style.css" />
+<style>
+<body>
+</body>
+
+
+
+</style>
+<center>
+<br>
+<img src="header.jpg" width="855" height="142.5" margin=10px solid green></embed></td>
+</center>
+
+
+<div id='div1' style="background: url(images/bg3.jpg) no-repeat top left;width: 1200px;
+  height: 30px;
+	margin: 0px auto;
+	padding: 0;"></div>
+
+
+
+
+
+
+
+
+</html>
+
+</center>
+ <script type='text/javascript' src="js1/jquery-1.3.2.min.js"></script>
+<script type='text/javascript' src='js/menu.js'></script>
+<link rel="stylesheet" href="style1.css" type="text/css"  />
+<style>
+
+#nav
+{
+
+background-color:#FFA500;
+float:center;
+font-size:17;
+width:auto;
+list-style:none;
+padding:9;
+position:absolute;
+top:200;
+left:450;
+
+
+}
+#nav li a
+{
+background-color:#5a0002;
+float:left;
+border-right:5px solid #617C58;
+padding:10;
+
+
+z-index:1;
+
+}
+#nav li a:hover
+{
+background-color:#FDD017;
+text-decoration:underline;
+color:#0000FF;
+
+
+}
+
+#sav
+{z-index:1;
+}
+</style>
+
+</style>
+
+
+<ul id="nav">
+    <li><a href="index.php" ><b> Home</b></a></li>
+    <li><a href="#"><b> Ganeshotsav </b></a>
+<ul id='sav'>
+                  <li><a href="about.php" ><b>About </b></a></li>
+<li><a href="#" ><b>History </b></a>
+<ul id='sav'>
+                    <li><a href="index.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girls.php"><b>Girls Hostel</b></a></li>
+					
+                   
+              </ul>
+
+					
+                   
+              </ul>
+    
+            
+    
+
+
+<li><a href='down.php?fname=events.docx'><b>Events</b></a></li>
+
+
+<li><a href='upload.php'><b>Photos</b></a>
+<ul id='sav'>
+                    <li><a href="upload.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girl.html"><b>Girls Hostel</b></a></li>
+					
+                   
+              </ul>
+
+
+</ul>
+
+					 </table>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+
+
+
+
+
+
+<br>
+<center>
+
+
+					 </table>
+
+<!DOCTYPE html>
+<html>
+	<head><title>Ganeshotsav 2KXI Photos</title>
+		<br><br><br><u><h1 style='color:red'>Ganeshotsav Photos</u></h1></u>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script src="js1/jquery-1.6.1.min.js" type="text/javascript"></script>
+		<!--script src="js1/jquery.lint.js" type="text/javascript" charset="utf-8"></script-->
+		<link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" charset="utf-8" />
+		<script src="js1/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+		
+		<style type="text/css" media="screen">
+			* { margin: 0; padding: 0; }
+			
+			body {
+				background: ;
+				font: 62.5%/1.2 Arial, Verdana, Sans-Serif;
+				padding: 0 20px;
+			}
+			
+			h1 { font-family: Georgia; font-style: italic; margin-bottom: 10px; }
+			
+			h2 {
+				font-family: Georgia;
+				font-style: italic;
+				margin: 25px 0 5px 0;
+			}
+			
+			p { font-size: 1.2em; }
+			
+			ul li { display: inline; }
+			
+			.wide {
+				border-bottom: 1px #000 solid;
+				width: 4000px;
+			}
+			
+			.fleft { float: left; margin: 0 20px 0 0; }
+			
+			.cboth { clear: both; }
+			
+			#main {
+				background: ;
+				margin: 0 auto;
+				padding: 30px;
+				width: 1000px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="main">
+			<h1 style='color:pink'>Boys Hostel.....</h1>
+			
+			
+			
+			<ul class="gallery clearfix">
+				<li><a href="image/7.JPG?lol=lol" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/7.jpg" width="60" height="60" alt="At the time of Ganesh Visarjan " /></a></li>
+				<li><a href="image/8.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/8.jpg" width="60" height="60" alt="" /></a></li>
+				<li><a href="image/9.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/9.jpg" width="60" height="60" alt="" /></a></li>
+				<li><a href="image/10.jpg" rel="prettyPhoto[gallery1]" title="Ganeshotsav 2kxii Boys Hostel"><img src="image/10.jpg" width="60" height="60" alt="" /></a></li>
+				<li><a href="image/12.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/12.jpg" width="60" height="60" alt="" /></a></li>
+<li><a href="image/1.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/1.jpg" width="60" height="60" alt="" /></a></li>
+<li><a href="image/2.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/2.jpg" width="60" height="60" alt="" /></a></li>
+<li><a href="image/3.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/3.jpg" width="60" height="60" alt="" /></a></li>
+<li><a href="image/4.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/4.jpg" width="60" height="60" alt="" /></a></li>
+<li><a href="image/5.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/5.jpg" width="60" height="60" alt="" /></a></li>
+
+<li><a href="image/7.jpg" rel="prettyPhoto[gallery1]" title="Last Year Ganeshotsav  Boys Hostel Photos"><img src="image/7.jpg" width="60" height="60" alt="" /></a></li>
+				
+			</ul>
+
+<br><br>
+<h1  style='color:white;font:30'>Ganeshotsav 2kxii photoswill be updated soon.....</H1>
+
+
+			
+			<script type="text/javascript" charset="utf-8">
+			$(document).ready(function(){
+				$("area[rel^='prettyPhoto']").prettyPhoto();
+				
+				$(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true});
+				$(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
+		
+				$("#custom_content a[rel^='prettyPhoto']:first").prettyPhoto({
+					custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
+					changepicturecallback: function(){ initialize(); }
+				});
+
+				$("#custom_content a[rel^='prettyPhoto']:last").prettyPhoto({
+					custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
+					changepicturecallback: function(){ _bsap.exec(); }
+				});
+			});
+			</script>
+</center>
+	
+			
+	</div>
+	</body>
+</html>
+
+
+
+
+
