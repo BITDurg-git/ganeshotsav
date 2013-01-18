73adc5b6f93a75908bc280ea9edc0ad5c191ebd6
Sourabh Kumar Agrawal <sourabhbitian@gmail.com>
2013-01-18 02:51:36 -0800

    Create girls.php

---
+++ girls.php
@@ -0,0 +1,281 @@
+    <script type='text/javascript' src='js/jquery-1.2.3.min.js'></script>
+<script type='text/javascript' src='js/menu.js'></script>
+<link rel="stylesheet" href="style1.css" type="text/css"  />
+<style>
+
+#nav
+{
+
+background-color:#FFA500;
+float:center;
+font-size:17;
+width:auto;
+list-style:none;
+padding:9;
+position:absolute;
+top:460;
+left:270;
+
+
+}
+#nav li a
+{
+background-color:#5a0002;
+float:left;
+border-right:5px solid #617C58;
+padding:10;
+
+
+z-index:1;
+
+}
+#nav li a:hover
+{
+background-color:#FDD017;
+text-decoration:underline;
+color:#0000FF;
+
+
+}
+
+#sav
+{z-index:1;
+}
+</style>
+
+</style>
+</style>
+
+
+<ul id="nav">
+    <li><a href="index.php" ><b> Home</b></a></li>
+    <li><a href="#"><b> Ganeshotsav </b></a>
+<ul id='sav'>
+                  <li><a href="about.php" ><b>About </b></a></li>
+<li><a href="#" ><b>History </b></a>
+<ul id='sav'>
+                    <li><a href="index.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girls.php"><b>Girls Hostel</b></a></li>
+  				
+                   
+              </ul>
+
+					
+                   
+              </ul>
+    
+            
+    
+
+
+<li><a href='down.php?fname=events.docx'><b>Events</b></a></li>
+
+
+<li><a href='upload.php'><b>Photos</b></a>
+<ul id='sav'>
+                    <li><a href="upload.php" ><b>Boys Hostel</b></a></li>
+                 <li><a href="girl.html"><b>Girls Hostel</b></a></li>
+					
+                   
+              </ul>
+
+
+</ul>
+
+					 </table>
+
+
+
+		
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>Ganeshotsava 2kxii</title>
+
+		<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery.gallerax-0.2.js"></script>
+		
+</head>
+<body>
+   
+<br>
+<center>
+<embed src="web_ganesh.swf" width="855" height="142.5" margin=10px solid green></embed></td>
+</center>
+                
+            </tr>
+        </table></div>
+		</div>	
+	</div>
+</div>
+
+<div id="slideshow">
+	<div id="slideshow_inner">
+		<div class="content">
+		
+				<div id="thumbnails">
+					<ul>
+						<li><a href="#"><img src="photo/1.jpg" alt="" title="Ganeshotsav 2kxii;  Girls Hostel; Enjoying Girls on Ganesh Visarjan" class="active" /></a></li>
+
+						<li><a href="#"><img src="photo/2.jpg" alt="" title="Ganeshotsav 2kxii;  Girls Hostel;Poster Competition" /></a></li>
+						<li><a href="#"><img src="photo/3.jpg" alt="" title="Ganeshotsav 2kxii;Girls Hostel" /></a></li>
+						<li><a href="#"><img src="photo/4.jpg" alt="" title="Ganeshotsav 2kxii; Girls Hostel;" /></a></li>
+						<li><a href="#"><img src="photo/5.jpg" alt="" title="Ganeshotsav 2kxii;Girls Hostel  " /></a></li>
+						<li><a href="#"><img src="photo/6.jpg" alt="" title="Ganeshotsav 2kxii;Girls Hostel; <b>  </b>"/></a></li>
+	
+</ul>
+					
+				</div>
+				
+				<div id="container">
+					<div class="container_inner"><a href="#" title=""><img src="images/emi1.jpg" alt="" /></a></div>
+				</div>
+				
+				<div id="toppost">
+					<h2><a href="" title=""><span class="line"></span></a></h2>
+					<p class="date line2"></p>
+					<span class="line3">
+						
+					</span>
+					<p class="excerpt"><a href="" title=""></a></p>
+				</div>
+				
+				
+					<script type="text/javascript">
+
+						$.gallerax({
+							outputSelector: 		'#container img',				
+							thumbnailsSelector:		'#thumbnails li img',		
+							captionSelector:		'#toppost .line',			
+							captionLines:			3,							
+							fade: 					'fast',					
+							advanceFade:			'slow',					
+							advanceDelay:			4000,						
+							advanceResume:			12000,					
+
+						});
+
+					</script>				
+				
+				<div style="clear: both;">&nbsp;</div>
+		
+		</div>	
+	</div>
+</div>
+
+
+<div id="content_wrapper">
+	<div id="content">
+		<div class="content">
+		
+			<div id="post">
+				<p class="date"></p>
+				<h2><a href="#" title=""></a></h2>
+
+				<div class="postimg"><div class="postimg_inner"><a href="#" title=""><img src="images/ganesha.jpg" width='600' height='350' alt="" /></a></div></div>
+				<br />
+
+				
+				<p class="excerpt"><a href="about.php" title="about more...."><b>Read more......</b></a></p>
+			</div>
+			
+			<div id="sidebar">
+	</a></marquee>
+			
+<h1 align='center'>Live News</h1>
+<div id='bigad' style="background-image:url(images/300x250.jpg);padding:5px;width:300px;height:200px;border:1px solid black;">
+<marquee direction='up' onmouseover="this.stop()" onmouseout="this.start()" scrollamount="2" direction="up" width="100%" height="" align="center"> <b>Live news from BIT-Hostel.....<br>
+<br /> <b>Poster Making Competition</b> 
+(open for all Boys & Girls)<br />
+Last Date Of Submission: 17-09-2012</b>
+<img src='image/poster.jpg' width='100' height='100'>
+<img src='image/poster1.jpg' width='100' height='100'>
+</b>
+
+
+</marquee>
+</marquee>
+</div></ul>
+<br>
+<br>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=upload/index.php";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+
+
+
+
+<center>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="https://www.facebook.com/pages/Ganeshotsav-2kxii/375197382549198?sk=info" data-send="true" data-width="250" data-height="250"data-show-faces="true"></div>
+</center>
+
+
+
+
+
+
+							
+			
+			
+			</div>
+			<div style="clear: both;">&nbsp;</div>
+		
+		</div>
+	</div>
+</div>
+
+<div id="footer">
+
+	<div class="inner"></div>
+		<div class="content">
+
+<div align='center'><img src='http://www.hit-counts.com/counter.php?t=27&digits=5&ic=0&cid=1141533' border='0' alt=''></a><BR></div>
+
+
+		
+		<b><div style="text-align: center;"><u>Designed By</u><Br><a href="http://www.facebook.com/sourabhbitian" title="Sourabh Kumar Agrawal">Sourabh Kumar Agrawal</a></b></div></b>
+</body>    
+</html>
+
+
+
+
+
+
+
+
+    
+
+
+<html>
+
+
+
+
+
+
+
+
+    
+
+
